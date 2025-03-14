c7024b8123b55b1f8412004c96ad1d9c3ac1c7c6
PureunKang <pureun0000@gmail.com>
2025-03-14 12:01:08 +0900

    타입 정의 수정

--- src/types/intro.ts
+++ src/types/intro.ts
@@ -1,7 +1,5 @@
 export type Intro = {
-  intro: {
-    description1: string;
-    description2: string;
-    description3: string;
-  };
+  description1: string;
+  description2: string;
+  description3: string;
 };
